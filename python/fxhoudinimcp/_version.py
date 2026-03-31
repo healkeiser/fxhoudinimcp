9ea7771759d5dfdc7710d9c36e01fd435488147a
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2026-03-31 20:51:37 +0200

    [FEAT] Versionning system

--- python/fxhoudinimcp/_version.py
+++ python/fxhoudinimcp/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.3.1.post1.dev6+g118fc2b8b.d20260331'
-__version_tuple__ = version_tuple = (0, 3, 1, 'post1', 'dev6', 'g118fc2b8b.d20260331')
+__version__ = version = '1.0.0'
+__version_tuple__ = version_tuple = (1, 0, 0)
 
 __commit_id__ = commit_id = None
